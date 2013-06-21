e22be5e3cce8a1b8d9289728275d4063c65b9e3c
Kishore Manoharan <Kishore.mano87@gmail.com>
2013-06-21 15:05:51 +0530

    Added Event Page controller

---
+++ app/controllers/event_pages_controller.rb
@@ -0,0 +1,10 @@
+class EventPagesController < ApplicationController
+  def home
+  end
+
+  def events
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventManager::Application.config.secret_token = 'eb078f678cd1a1d45506d98c1ceda066677c9edf351d8c76ea92a82e748b9df2ae1367afaed0a5e9618eba03e064e278b0df965d8f3e912a86ecd84646a640f4'
